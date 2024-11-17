0d0792f41e26a642d91782666091c4957b118806
andrewbinkard <andrewbinkard@gmail.com>
2024-11-16 19:36:59 -0600

    WRM-5: test css in vercel deploy

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import "./main.module.scss"; // Import global styles
+import "./main.scss"; // Import global styles
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
