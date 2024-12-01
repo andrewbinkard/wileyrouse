90da974e0bdbc47772af5261988ff72015babe9d
andrewbinkard <andrewbinkard@gmail.com>
2024-11-30 18:24:01 -0600

    WRM-10: wip carousel2

--- src/Features/Rouse/Rouse.tsx
+++ src/Features/Rouse/Rouse.tsx
@@ -1,30 +1,10 @@
 import { FC } from "react";
-import { Link, Routes, Route } from "react-router-dom";
-import RouseAcknowledgements from "./RouseAcknowledgements";
-import RousePrivateLessonFaculty from "./RousePrivateLessonFaculty";
-import CandidCarousel from "../../Components/Shared/ImageCarousel";
 import styles from "./Rouse.module.scss";
 
 const Wiley: FC = () => {
   return (
     <div className={styles.container}>
-      <h2>Rouse House</h2>
-
-      {/* Navigation Links for Wiley's subpages */}
-
-      <Link to="acknowledgements">Acknowledgements</Link>
-      <Link to="private-lesson-faculty">Private Lesson Faculty</Link>
-
-      {/* Routes for Wiley's subpages */}
-      <Routes>
-        <Route path="acknowledgements" element={<RouseAcknowledgements />} />
-        <Route
-          path="private-lesson-faculty"
-          element={<RousePrivateLessonFaculty />}
-        />
-      </Routes>
-
-      <CandidCarousel />
+      <h2>Under Construction</h2>
     </div>
   );
 };
