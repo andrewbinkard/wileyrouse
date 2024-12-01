dcb03b21e3e66e8645b8a51a5cab3ef5ab6592bc
andrewbinkard <andrewbinkard@gmail.com>
2024-11-30 23:17:53 -0600

    WRM-12: update composers

--- src/Features/Wiley/WileyComposers/const.ts
+++ src/Features/Wiley/WileyComposers/const.ts
@@ -1,29 +1,102 @@
 import { ComposerCardProps } from "../../../Components/Shared/ComposerCard/types";
-import JSBach from "../../../assets/images/js_bach.jpeg";
-import YasminWilliams from "../../../assets/images/YASMIN+WILLIAMS+Acadia.jpeg";
+import ShuheiTamura from "../../../assets/images/Tamura.jpg";
+import LauraEstes from "../../../assets/images/LauraEstes.jpeg";
+import RobertWSmith from "../../../assets/images/RobertWSmith.jpg";
+import BrianBalmages from "../../../assets/images/BrianBalmages.jpeg";
+import AdrianSims from "../../../assets/images/AdrianSims.jpeg";
+import KevinMixon from "../../../assets/images/KevinMixon.jpg";
+import YukikoNishimura from "../../../assets/images/YukikoNishimura.jpg";
+import TylerSGrant from "../../../assets/images/TylerSGrant.jpeg";
+import RandallStandridge from "../../../assets/images/RandallStandridge.jpg";
 
 export const composerData: ComposerCardProps[] = [
   {
-    name: "Johann Sebastian Bach",
-    imgSrc: JSBach,
-    website: "https://www.britannica.com/biography/Johann-Sebastian-Bach",
-    details: `Johann Sebastian Bach was born to Johann Ambrosius Bach and Maria Elisabeth Lämmerhirt on March 21, 1685. Johann Sebastian Bach was born to Johann Ambrosius Bach and Maria Elisabeth Lämmerhirt on March 21, 1685. Johann Sebastian Bach was born to Johann Ambrosius Bach and Maria Elisabeth Lämmerhirt on March 21, 1685. 
-    
-    He was born in the small town of Eisenach, in Thuringia, Germany. He was born in the small town of Eisenach, in Thuringia, Germany. He was born in the small town of Eisenach, in Thuringia, Germany. 
-    
-    Bach was introduced to music at an early age and is known as one of the most talented musicians and composers of the 17th century.
-    
-    Test test test test.`,
+    name: "Shuhei Temura",
+    imgSrc: ShuheiTamura,
+    website: "https://www.windrep.org/Shuhei_Tamura",
+    details: `Shuhei Tamura (b. 1986, Okayama City, Japan) is a Japanese composer. Mr. Tamura graduated from Tokyo University of the Arts, Department of Music, School of Music, and completed a master’s degree in pedagogy from Tokyo Gakugei University with an emphasis in music education.
+
+Tamura is an arranger as well as a composer who has written for orchestras, choruses, and brass bands. He is most prolific in the latter category, with a large number of published scores in this area. Tamura’s music is heavily influenced by Western music forms that have become the foundation of Japanese instrumental music traditions. These hybrid musical aesthetics, with the replication of Western forms, emerged through intercultural transmission (teaching and learning of a foreign tradition) and music acculturation (mastery of a foreign genre and embracing it as part of one’s own culture).
+
+He took third place in the 17th Music Collection Nippon Kyokyou Competition Composition Division (General Section), and was first in the Osamu Composition Competition (cum audience prize).`,
   },
 
   {
-    name: "Yasmin Williams",
-    imgSrc: YasminWilliams,
-    website: "https://www.yasminwilliamsmusic.com/",
-    details: `When guitarist and composer Yasmin Williams sits down to compose music, she doesn’t scour her subconscious for unheard melodies or clever chord progressions. Instead, she goes granular—fixating on a single note. She’ll play it over and over, sustaining it, varying the attack or the release to change its essence, eventually adding notes to form chords.
+    name: "Laura Estes",
+    imgSrc: LauraEstes,
+    website: "https://www.lauraestesmusic.com/",
+    details: `After 36 years as a middle and high school band director, Laura Estes (b. 1959) retired in 2018 from the Cobb County School System (GA) and began composing. Her first piece, Kvetchers (Surprises in Controversial Time), was published in Excelcia Music Publishing’s 2020 catalog, and with her composition Walking on Air, Estes was named the winner of the 2021 Women Band Director’s International (WBDI) Composition Competition. Along with original music performed at The Midwest Clinic in Chicago, Estes’ concert works are frequently featured at state music conventions, All-State events, and regional honor bands throughout the United States. In addition to appearing on state festival and contest lists, several of Estes’ band and orchestra compositions have also been designated as J.W. Pepper Editor’s Choice.
+
+A recipient of the National Band Association’s Citation of Excellence, Estes’ bands consistently earned Superior ratings at performance evaluations throughout her career, and middle and high school bands under her direction were selected as guest performers at clinics and conventions in the Southeast. Estes has also presented at state conventions, on college campuses, and at the Midwest Clinic.
+
+A South Florida native, Estes earned her bachelor’s degree in music education from The Florida State University, and her master’s degree in music education from Georgia State University. In addition to composing, she continues to serve as an adjudicator, guest conductor, clinician, and mentor. Estes and her husband Gil reside in Marietta, GA, and have 5 amazing children, along with a host of equally amazing grandchildren.`,
+  },
+  {
+    name: "Robert W. Smith",
+    imgSrc: RobertWSmith,
+    website: "https://rwsmusic.com/composers/robert-w-smith/",
+    details: `Robert W. Smith (1958-2023) was one of the most popular and prolific composers in America today. He has over 600 publications in print with the majority composed and arranged through his long association with Warner Bros. Publications and the Belwin catalog.
+
+Mr. Smith’s credits include many compositions and productions in all areas of the music field. His original works for winds and percussion have been programmed by countless military, university, high school, and middle school bands throughout the United States, Canada, Europe, Australia, South America and Asia. His Symphony #1 (The Divine Comedy), Symphony #2 (The Odyssey), Symphony #3 (Don Quixote), Inchon and Africa: Ceremony, Song and Ritual have received worldwide critical acclaim. His educational compositions such as The Tempest, Encanto, and The Great Locomotive Chase have become standards for developing bands throughout the world.
+
+Mr. Smith’s music has received extensive airplay on major network television as well as inclusion in multiple motion pictures. From professional ensembles such as the United States Navy Band, United States Air Force Band, Boston Pops and the Atlanta Symphony to school bands and orchestras throughout the world, his music speaks to audiences in any concert setting. As a conductor, clinician and keynote speaker, Mr. Smith performed throughout North America, Asia, South America, Europe and Australia. His music has been recorded by various ensembles and is available on CD and download through iTunes, Amazon, and other recorded music outlets.
+
+Mr. Smith was the President/CEO of RWS Music Company, exclusively distributed through Alfred Music. In addition, he taught in the Music Industry program at Troy University in Troy, Alabama. His teaching responsibilities were focused in music composition, production, publishing and business.`,
+  },
+  {
+    name: "Brian Balmages",
+    imgSrc: BrianBalmages,
+    website: "https://www.brianbalmages.com/",
+    details: `Brian Balmages is known worldwide as a composer and conductor who equally spans the worlds of orchestral, band, and chamber music. His music has been performed by groups ranging from professional symphony orchestras to elementary schools in venues such as Carnegie Hall, the Kennedy Center, Sydney Opera House, Toronto Centre for the Arts, and many more. He is a recipient of the A. Austin Harding Award from the American School Band Directors Association, won the 2020 NBA William D. Revelli Composition Contest with his work Love and Light, and was awarded the inaugural James Madison University Distinguished Alumni Award from the School of Visual and Performing Arts. In the same year, he was commissioned by his other alma mater, the University of Miami, to compose music for the inauguration of the institution’s 6th president, Dr. Julio Frenk. His music was also performed as part of the 2013 Presidential Inaugural Prayer Service, which was attended by both President Obama and Vice President Biden.
+
+    As a conductor, Mr. Balmages enjoys regular engagements with all-state and regional ensembles as well as university and professional groups throughout the world. Notable guest conducting appearances have included the Midwest Clinic, Western International Band Clinic, Maryborough Music Conference (Australia), College Band Directors Conference, American School Band Directors Association National Conference, numerous state ASTA conferences, Teatro dell’Aquila (Italy), and others. He is an elected member of the American Bandmasters Association and has taught instrumental conducting at Towson University where he also served as Assistant Director of Bands and Orchestras. Currently, he is Director of MakeMusic Publications and Digital Education for Alfred and MakeMusic.`,
+  },
+  {
+    name: "Adrian B. Sims",
+    imgSrc: AdrianSims,
+    website: "https://www.adrianbsims.com/",
+    details: `Adrian B. Sims (b. 2000), born in Seattle, Washington is an accomplished composer, conductor, educator, and trombonist. Adrian’s music has been performed at prestigious conferences such as The Midwest Band and Orchestra Clinic and at new music reading sessions across the country. His music has also been selected for the Bandworld Top 100 List, and many of his works appear on the J.W. Pepper Editor’s Choice List. Adrian has also been selected as a winner in multiple composition competitions including the Maryland Music Educators Association Young Composers Project and the Make Music Young Composers Contest. He is frequently invited to rehearse, conduct, and lead clinics with bands and orchestras across the United States. Adrian is also active as an educator in the Maryland area and has worked with a variety of programs including the Baltimore County Summer Music Camp, Terrapin Music Camp at the University of Maryland, and the DC Youth Orchestra Programs.
+
+Adrian is a graduate of The University of Maryland with degrees in Music Education and Composition. He is currently pursuing a Master’s Degree in Composition at The University of Texas at Austin. As a trombonist, he has performed in a wide variety of musical ensembles including pit and symphony orchestras as well as jazz and concert bands. Adrian studied trombone with Matthew Guilford, solo bass trombonist with the National Symphony Orchestra, and Aaron LaVere, principal trombonist with the Baltimore Symphony Orchestra. He is grateful for the continued support from his middle and high school band directors Cindy Stevenson and Christopher Gnagey. He credits Dr. Robert Gibson, Professor of Composition at The University of Maryland, and world-renowned composer Brian Balmages for his development as a composer.`,
+  },
+  {
+    name: "Kevin Mixon",
+    imgSrc: KevinMixon,
+    website: "https://www.alfred.com/authors/kevin-mixon/",
+    details: `Kevin Mixon teaches instrumental music at Blodgett K-8 School, Syracuse City Schools, New York. He regularly presents clinics internationally at venues such as The International Society for Music Education Convention, The Midwest International Band and Orchestra Clinic, Music Educators National Conference events, and numerous state and regional music conferences. His current clinic instrumental music education topics include urban and rural schools, special learners, curriculum integration, multicultural music, alternative rehearsal techniques, and percussion pedagogy.
     
-    She has a name for this. She calls it “ruminating” and describes it as a key part of her writing. “I’ve learned a little about how to sit with a note, and to give things time,” the Virginia native says. “You find some tiny idea and just play it over and over again until something else pops up … You have to trust that sometimes a note will take you to where it wants to go next.”
-    
-    This intuitive process led Williams to the breathtakingly tactile and rivetingly understated Acadia, her Nonesuch debut. Its nine original songs expand, dramatically, on the sonic space Williams created with her acclaimed 2021 album, Urban Driftwood. In addition to the crisp fingerpicked guitar that helped establish her as a fast-rising star of instrumental folk, Williams plays kora, harp guitar, banjo, and electric guitar and bass—all with authority. And where her two previous records have been mostly solo, Acadia finds Williams collaborating with artists across a wide stylistic range, including the vocalist Aoife O’Donovan, violinist Darian Donovan Thomas, the folk quartet Darlingside, synthesist Rich Ruth, and jazz alto saxophonist Immanuel Wilkins.`,
+    He holds degrees, summa cum laude, from Onondaga Community College, Syracuse University, and the University of Illinois (Urbana-Champaign), and is currently completing a Certificate of Advanced Studies in Educational Leadership. Several of his articles have appeared in Music Educators Journal, Teaching Music, The Instrumentalist, and The National Band Association Journal. He is author of Reaching and Teaching All Instrumental Music Students and co-author of Teaching Music in the Urban Classroom (MENC/Rowman & Littlefield Publications). Mr. Mixon is also an accomplished composer and arranger with several published works for band and orchestra.`,
+  },
+  {
+    name: "Yukiko Nishimura",
+    imgSrc: YukikoNishimura,
+    website: "https://www.carlfischer.com/yukiko-nishimura",
+    details: `Yukiko Nishimura, Composer/Pianist, was born in Japan. She graduated from Tokyo National University of Arts in 1990. In 1991, she began private study with Dr. Alfred Reed at the University of Miami and in 1993 she continued her studies with Dr. Richard Danielpour at Manhattan School of Music. Among her honors, are the special mention at the 15th and 26th International Competition for Original Composition for band in Corciano, Italy and the 6th Aoyama Award. Since 1996, she has given concerts consisting entirely of her own music in Japan. She gave a solo piano recital at New York Public Library, Donnell Library Center in 2003. In 2005, the commissioned work for the first silent film in the United States “Edison’s 1910 Frankenstein” for string quartet was premiered in Gettysburg College in Pennsylvania by Covington String Quartet. The music for the silent film “The Water Magician” directed by Kenji Mizoguchi in 1933 was premiered in 2007. In 2010, a theatrical work “Fantasy of Kenji” based on the stories by Kenji Miyazawa was premiered. In 2014, she collaborated with Noh play, which is Japanese traditional theater work, as a composer and a pianist.
+
+In 2017, her piano work was performed by America’s famous pianist Sara Davis Buechner at Carnegie Hall’s Weille Recital Hall. The work got some good reviews on The New York Times, New York Concert Reviews, and more. Her work was nominated in the category of Contemporary Classical/Instrumental for “Hollywood Music in Media Award” this year.
+
+Ms. Nishimura has received numerous commissions and has composed for piano, marimba, percussion, chamber music, band music, music for string instruments and orchestra. She has also been active as a pianist and an arranger. She has studied composition with Atsutada Otaka, Dr. Alfred Reed, Dr. Richard Danielpour, and Giampaolo Bracali. She studied piano with Ivan Davis and Dr. Sara Davis Buechner.`,
+  },
+  {
+    name: "Tyler S. Grant",
+    imgSrc: TylerSGrant,
+    website: "https://www.tylersgrant.com/",
+    details: `Tyler S. Grant is an internationally recognized composer, arranger, conductor, and clinician of music for concert bands, orchestras, and chamber ensembles of all levels. His music is featured regularly in performances by both region and all state honor ensembles and is also featured annually on Editor’s or Publisher’s Choice Repertoire Lists around the world. His work, Panoramic Fanfare, was a winner of the Dallas Wind Symphony’s 2014 “Call for Fanfares” Competition. Panoramic Fanfare has since been performed by collegiate and professional ensembles throughout North America.
+
+Since 2010, Tyler has regularly accepted commissions for new works and produced new publications for bands and orchestras each year. He credits his mentor Brian Balmages as pivotal to his success as a composer.  Notable performance venues of his works include The Midwest Clinic, Carnegie Hall, Music for All’s National Concert Band Festival, the Meyerson Symphony Center, the Macy’s Thanksgiving Day Parade, Bands of America Grand Nationals, and numerous state-wide, national, and international music conferences and conventions. 
+
+In addition to composing and arranging, Tyler enjoys conducting and clinic engagements with ensembles throughout the United States. He has served as a clinician with over 100 school instrumental music programs and honor groups across more than 15 states. Tyler was a contributing author for Alfred Music Publishing Company’s Sound Innovations: Ensemble Development series and has contributed interviews to numerous podcasts and articles including School Band & Orchestra Magazine and The Instrumentalist. Notable conducting venues include The Midwest Clinic and all-state bands in Nebraska, Oregon, Alabama, Louisiana, South Dakota, and North Dakota.
+
+Tyler is a graduate of the University of Alabama where he earned a bachelor’s degree in instrumental music education and studied conducting with Professor Randall Coleman. Before becoming a full-time composer and arranger, Tyler served as the Director of Bands at Holy Innocents’ Episcopal School from 2019-2023. He currently resides in Atlanta, Georgia, and maintains an active composing and conducting schedule. His published works are available through The FJH Music Company, Inc., Alfred/MakeMusic Publications, and Tyler S. Grant Music Works, LLC.`,
+  },
+  {
+    name: "Randall Standridge",
+    imgSrc: RandallStandridge,
+    website: "https://randallstandridge.com/",
+    details: `Randall Standridge (b.1976) received his Bachelor's of Music Education from Arkansas State University. During this time, he studied composition with Dr. Tom O'Connor, before returning to Arkansas State University to earn his Master's in Music Composition, studying with Dr. Tom O'Connor and Dr. Tim Crist. In 2001, he began his tenure as Director of Bands at Harrisburg High School in Harrisburg, Arkansas. He left this post in 2013 to pursue a career as a full-time composer and marching arts designer.
+
+Mr. Standridge's music is performed internationally. He has had numerous works selected for the J.W. Pepper's editor's choice. His compositions Snake Charmer, Gently Blows the Summer Wind, and Angelic Celebrations have been included in the "Teaching Music Through Performance in Band" series. He has had numerous works performed at the prestigious Midwest Clinic in Chicago, Illinois. The Arkansas State University Wind Ensemble premiered his work Art(isms) at the 2010 CBDNA conference in Las Cruces, New Mexico, and his work Stonewall: 1969 was premiered at the National LGBA conference in 2019. His Symphony no.1: A Ghost Story was premiered in 2023.  In addition, Mr. Standridge's "unBroken Project," a musical initiative about mental health, has received widespread acclaim for its musical content and for providing opportunities to normalize discussions about mental health for music students and audiences.
+
+In addition to his career as a composer, Mr. Standridge is the owner and editor of Randall Standridge Music, LLC and Grand Mesa Marching. He is in demand as an arranger/designer for the marching arts.  He lives in Jonesboro, Arkansas, with his husband, Steven, and their very, very spoiled pets.`,
   },
 ];
