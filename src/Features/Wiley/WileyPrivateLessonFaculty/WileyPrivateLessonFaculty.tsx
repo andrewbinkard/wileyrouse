3d865c41d662357d494e847b9f4089e1022b3d97
andrewbinkard <andrewbinkard@gmail.com>
2024-12-03 12:55:54 -0600

    WRM-23: finalize PL teacher content for wiley and rouse

--- src/Features/Wiley/WileyPrivateLessonFaculty/WileyPrivateLessonFaculty.tsx
+++ src/Features/Wiley/WileyPrivateLessonFaculty/WileyPrivateLessonFaculty.tsx
@@ -1,6 +1,6 @@
 import DirectorCard from "../../../Components/Shared/DirectorCard";
 import Footer from "../../../Components/Shared/Footer";
-import { privateLessonTeacherData } from "./const";
+import { privateLessonTeacherData } from "../../SharedData/privateLessonFaculty";
 import styles from "./WileyPrivateLessonFaculty.module.scss";
 
 const WileyPrivateLessonFaculty = () => {
@@ -8,16 +8,21 @@ const WileyPrivateLessonFaculty = () => {
     <div className={styles.container}>
       <h1>Private Lesson Faculty</h1>
       <div className={styles.facultyContainer}>
-        {privateLessonTeacherData.map(({ name, instrument, imgSrc }, idx) => {
-          return (
-            <DirectorCard
-              key={idx}
-              name={name}
-              instrument={instrument}
-              imgSrc={imgSrc}
-            />
-          );
-        })}
+        {privateLessonTeacherData
+          .filter(({ wiley }) => {
+            return wiley;
+          })
+          .map(({ name, instrument, imgSrc, bio }, idx) => {
+            return (
+              <DirectorCard
+                key={idx}
+                name={name}
+                instrument={instrument}
+                imgSrc={imgSrc}
+                bio={bio}
+              />
+            );
+          })}
       </div>
       <Footer />
     </div>
