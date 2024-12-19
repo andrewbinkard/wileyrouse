b3bef62d6c835770cee7109d993498e141977bc3
andrewbinkard <andrewbinkard@gmail.com>
2024-12-19 10:18:00 -0600

    add vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import "./main.scss"; // Import global styles
+import "./main.scss";
+import { inject } from "@vercel/analytics";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
+inject();
 
 root.render(
   <React.StrictMode>
