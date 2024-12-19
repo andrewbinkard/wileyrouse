1fbcff1bad39f58e1d7dc2b54bf250cbd9f101f2
andrewbinkard <andrewbinkard@gmail.com>
2024-12-19 17:18:05 -0600

    flip landing page links

--- src/Pages/const.ts
+++ src/Pages/const.ts
@@ -4,17 +4,17 @@ import wileyLogo from "../assets/images/WileyLogo.png";
 
 export const performingSchoolData: SchoolCardWithRoute[] = [
   {
-    schoolName: "Rouse High School",
-    ensembleName: "Honor Band",
-    imgSrc: rouseLogo,
-    performanceDetails: "3:00PM | Thursday, 12/19 | 375AB",
-    route: "/rouse",
-  },
-  {
     route: "/wiley",
     schoolName: "Wiley Middle School",
     ensembleName: "Wind Ensemble",
     imgSrc: wileyLogo,
     performanceDetails: "8:30AM | Friday, 12/20 | 375E",
+  },
+  {
+    schoolName: "Rouse High School",
+    ensembleName: "Honor Band",
+    imgSrc: rouseLogo,
+    performanceDetails: "3:00PM | Thursday, 12/19 | 375AB",
+    route: "/rouse",
   },
 ];
